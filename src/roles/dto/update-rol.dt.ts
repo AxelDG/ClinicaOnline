57cdd8104e1855d222bb45281f013c7d50590e8a
Axel Godardo <axel.godardo@municipiodetandil.edu.ar>
2023-10-04 21:15:11 -0300

    Modified backend

--- src/roles/dto/update-rol.dt.ts
+++
@@ -1,8 +0,0 @@
-export class UpdateRolDto {
-  public idRol?: number;
-  public administrador?: boolean;
-  public usuario?: boolean;
-  public medico?: boolean;
-}
-
-export type PartialUpdateUsuarioDto = Partial<UpdateRolDto>;
